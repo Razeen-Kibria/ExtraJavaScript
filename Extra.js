bc9bf257ce091edba9890a66fbfa4d6d3997a6c4
MD.Razeen Kibria <razeenkibriaofficial@gmail.com>
2024-10-05 15:15:37 +0600

    dedicated my curse

--- Extra.js
+++ Extra.js
@@ -1,3 +1,5 @@
+//--------------task-01-----------------
+/*
 function animalCalculator(depth) {
   var animal = 0;
   if (depth <= 10) {
@@ -18,3 +20,23 @@ function animalCalculator(depth) {
 }
 var totalAnimals = animalCalculator(22);
 console.log(totalAnimals);
+*/
+
+//----------------------Task-02-----------Functions---------------------
+
+function explain_CallBack(fullName, age, task) {
+  console.log("Hello", fullName);
+  console.log("your age:", age);
+  task();
+}
+
+function handWash() {
+  console.log("Wash your hand properly ");
+}
+
+function takeShower() {
+  console.log("Plz take a shower because you are beautiful!");
+}
+
+explain_CallBack("Razeen kibria", 23, handWash);
+explain_CallBack("Sayma sultana", 19, takeShower);
